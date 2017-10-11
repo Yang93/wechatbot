c795573ba9af25926aa79941152fcce1357c4b32
Mengyang Hong <mengyang.hong@gmail.com>
2017-10-11 18:54:30 -0400

    add in Darah's code

---
+++ wechatbot.rb
@@ -0,0 +1,28 @@
+require 'sinatra'
+require "sinatra/reloader" if development?
+
+enable :sessions
+
+get '/wechat/incoming' do
+    signature = params['signature'] || ''
+    timestamp = params['timestamp'] || ''
+    nonce = params['nonce'] || ''
+    echostr = params['echostr'] || ''
+
+    # This is the token you
+    # add to the setup on WeChat
+    # replace the word TOKEN with whatever
+    # you enter
+    token = mealorderhelper;
+
+    # Compute the signature (note that the shared token is used too)
+    verification_elements = [@token, timestamp, nonce]
+    verification_elements = verification_elements.sort
+    verification_string = verification_elements.join('')
+    verification_string = Digest::SHA1.hexdigest(verification_string)
+    # If the signature is correct, output the same "echostr" provided by the WeChat server as a parameter
+    if signature == verification_string
+      return echostr
+    end
+    return ''
+end
